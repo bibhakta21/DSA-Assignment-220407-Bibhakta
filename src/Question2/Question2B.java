3081fb793890d5e5b648dc794b91bf0ab904a2c2
bibhakta21 <bibhaktalamsal8@gmail.com>
2024-02-19 20:35:04 +0545

    changed some code in question 2b

--- src/Question2/Question2B.java
+++ src/Question2/Question2B.java
@@ -1,116 +1,69 @@
-package Question2;
+//You are given an integer n representing the total number of individuals. Each individual is identified by a unique ID from 0 to n-1. The individuals have a unique secret that they can share with others.
+//
+//        The secret-sharing process begins with person 0, who initially possesses the secret. Person 0 can share the secret with any number of individuals simultaneously during specific time intervals. Each time interval is represented by a tuple (start, end) where start and end are non-negative integers indicating the start and end times of the interval.
+//
+//        You need to determine the set of individuals who will eventually know the secret after all the possible secret-sharing intervals have occurred.
+//
+//        Example:
+//
+//        Input: n = 5, intervals = [(0, 2), (1, 3), (2, 4)], firstPerson = 0
+//
+//        Output: [0, 1, 2, 3, 4]
+//
+//        Explanation:
+//        In this scenario, we have 5 individuals labeled from 0 to 4.
+//
+//        The secret-sharing process starts with person 0, who has the secret at time 0. At time 0, person 0 can share the secret with any other person. Similarly, at time 1, person 0 can also share the secret. At time 2, person 0 shares the secret again, and so on.
+//
+//        Given the intervals [(0, 2), (1, 3), (2, 4)], we can observe that during these intervals, person 0 shares the secret with every other individual at least once.
+//
+//        Hence, after all the secret-sharing intervals, individuals 0, 1, 2, 3, and 4 will eventually know the secret.
+//
 
 
+        package Question2;
 
-import java.util.ArrayList;
-import java.util.List;
+
+import java.util.*;
 
 public class Question2B {
-
-    public static List<Integer> eventualSecrets(int n, int[][] intervals, int firstPerson) {
+    // Method to determine the set of individuals who will eventually know the secret
+    public static List<Integer> determineSecretRecipients(int n, int[][] intervals, int firstPerson) {
+        // Initialize a boolean array to track whether each person knows the secret
         boolean[] knowsSecret = new boolean[n];
+        Arrays.fill(knowsSecret, false);
+
+        // Mark the first person as knowing the secret
         knowsSecret[firstPerson] = true;
 
-        List<Integer> result = new ArrayList<>();
+        // Iterate through each interval and update the knowsSecret array accordingly
         for (int[] interval : intervals) {
-            updateKnowledge(knowsSecret, interval);
+            int start = interval[0];
+            int end = interval[1];
+            for (int i = start; i <= end; i++) {
+                knowsSecret[i] = true;
+            }
         }
 
+        // Store the indices of individuals who know the secret
+        List<Integer> recipients = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             if (knowsSecret[i]) {
-                result.add(i);
+                recipients.add(i);
             }
         }
 
-        return result;
-    }
-
-    private static void updateKnowledge(boolean[] knowsSecret, int[] interval) {
-        int start = interval[0];
-        int end = interval[1];
-
-        for (int i = start; i <= end; i++) {
-            if (knowsSecret[i]) {
-                for (int j = start; j <= end; j++) {
-                    knowsSecret[j] = true;
-                }
-                break;
-            }
-        }
+        return recipients;
     }
 
     public static void main(String[] args) {
-        int n = 5;
-        int[][] intervals = {{0, 2}, {1, 3}, {2, 4}};
-        int firstPerson = 0;
+        // Example input
+        int n = 5; // Total number of individuals
+        int[][] intervals = {{0, 2}, {1, 3}, {2, 4}}; // Secret-sharing intervals
+        int firstPerson = 0; // Initial person possessing the secret
 
-        List<Integer> result = eventualSecrets(n, intervals, firstPerson);
-
-        System.out.println("Individuals who will eventually know the secret: " + result);
+        // Determine the secret recipients and print the result
+        List<Integer> result = determineSecretRecipients(n, intervals, firstPerson);
+        System.out.println("The individual who will know secrets are: "+result); // Output: [0, 1, 2, 3, 4]
     }
 }
-
-
-
-
-
-//import java.util.Arrays;
-//
-///*
-// * You are given an integer n representing the total number of individuals. Each individual is identified by a unique
-// * ID from 0 to n-1. The individuals have a unique secret that they can share with others.
-// *
-// * The secret-sharing process begins with person 0, who initially possesses the secret. Person 0 can share the secret
-// * with any number of individuals simultaneously during specific time intervals. Each time interval is represented by
-// * a tuple (start, end) where start and end are non-negative integers indicating the start and end times of the interval.
-// *
-// * You need to determine the set of individuals who will eventually know the secret after all the possible secret-sharing
-// * intervals have occurred
-// */
-//
-////Note : I am assuming that in each interval=(start,end) individuals can share with start-end+1 no of individuals
-//// and that in every overlapping interval the mutually exclusive time will only be accounted for sharing process
-//public class Question2B{
-//    int individuals[];
-//    int intervals[][];
-//
-//    Question2B(int noOfindividuals, int[][] intervalMatrix) {
-//        individuals = new int[noOfindividuals];
-//        for (int i = 0; i < noOfindividuals; i++) {
-//            individuals[i] = i;
-//        }
-//        intervals = intervalMatrix;
-//    }
-//
-//    int[] startSharing() {
-//        int secretKnowingIndividuals = 0;
-//        for (int i = 0; i < intervals.length; i++) {
-//            for (int j = 0; j < intervals[0].length; j++) {
-//                // Above note condition: if interval overlaps
-//                if (i > 0 &&
-//                        intervals[i - 1][intervals[0].length - 1] > intervals[i][j]) {
-//                    continue;
-//                }
-//                // In each interval only end-start+1 individuals will know secret
-//                secretKnowingIndividuals++;
-//            }
-//            // All individuals know the secret
-//            if (secretKnowingIndividuals >= individuals.length) {
-//                break;
-//            }
-//        }
-//
-//        // Return an array of secret knowing individuals
-//        return Arrays.copyOf(individuals, secretKnowingIndividuals);
-//    }
-//
-//    public static void main(String[] args) {
-//        Question2B share = new Question2B(5, new int[][] { { 0, 1 }, { 1, 3 }, { 2, 4 } });
-//        int[] secretKnowingIndividuals = share.startSharing();
-//        for (int i : secretKnowingIndividuals) {
-//            System.out.print(i + "\t");
-//        }
-//        System.out.println();
-//    }
-//
-//}
